9da5b0f02f8bea63bb9f343ea7f5673460979721
Rabab Handa <rabab@Rababs-MacBook-Air.local>
2022-03-19 19:50:51 -0400

    final commit for the branch

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -4,9 +4,6 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
-// Import data from cities.js
-let cityData = cities;
-
 // Coordinates for each point to be used in the line.
 let line = [
     [33.9416, -118.4085],
